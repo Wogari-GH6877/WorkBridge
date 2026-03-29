edba6a0b3f2dcb95f44fde323432f7306c93dff7
Wogari-GH6877 <wogariararsa0968@gmail.com>
2026-03-28 21:42:24 -0700

    add other routes

--- Backend/Routes/UserRoute.js
+++ Backend/Routes/UserRoute.js
@@ -1,10 +1,19 @@
 import express from "express"
-import { Login, SignUp } from "../Controllers/UserController.js";
+import { createJob, Login, SignUp ,applyToJob,getAllJobs,getJobApplications,updateApplicationStatus,getMyApplications,
+    getMyPostedJobs
+} from "../Controllers/UserController.js";
 import { loginLimiter, signupLimiter } from "../Middleware/RateLimiter.js";
+import userAuth from "../Middleware/userAuth.js";
 
 const userRoutes=express.Router();
 
 userRoutes.post("/sign-up",signupLimiter,SignUp);
 userRoutes.post("/login",loginLimiter,Login);
-
+userRoutes.post("/post-job",userAuth,createJob);
+userRoutes.post("/jobs/:jobId/apply", userAuth, applyToJob);
+userRoutes.get("/jobs", getAllJobs);
+userRoutes.get("/jobs/:jobId/applications", userAuth, getJobApplications);
+userRoutes.patch("/applications/:appId/status", userAuth, updateApplicationStatus);
+userRoutes.get("/jobs/my-applications", userAuth, getMyApplications);
+userRoutes.get("/my-posted-jobs", userAuth, getMyPostedJobs);
 export default userRoutes;
